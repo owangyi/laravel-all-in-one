2469e1caaee8231a2b72eab844fa36025c54e282
Yi <wangyiicloud@icloud.com>
2024-10-06 23:59:44 +0800

    Introduce PHPstan

---
+++ tmp/cache/PHPStan/09/63/0963c5f6ab5dfc0bc1fe3fc496f6248055f91e11.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+
+// variadic-method-PHPUnit\Framework\Assert-assertTrue-/Users/yi.wang/Practices/laravel-all-in-one/vendor/phpunit/phpunit/src/Framework/Assert.php
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => '1728100974-v4',
+   'data' => false,
+));
---
+++ tmp/cache/PHPStan/f3/2a/f32aee9467f3c50f69d889c2e6afd3f26fe4f95a.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+
+// variadic-method-Illuminate\Testing\TestResponse-assertStatus-/Users/yi.wang/Practices/laravel-all-in-one/vendor/composer/../laravel/framework/src/Illuminate/Testing/TestResponse.php
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => '1728100975-v4',
+   'data' => false,
+));
