57dec2993623483a981b460a3f63c99b3b0611f9
Yi <wangyiicloud@icloud.com>
2024-10-06 23:59:44 +0800

    Introduce PHPstan

---
+++ tmp/cache/PHPStan/18/5b/185b6ac5422eefaa18cfb906d1c33ddf6ef7da42.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+
+// variadic-method-Illuminate\Foundation\Testing\TestCase-createApplication-/Users/yi.wang/Practices/laravel-all-in-one/vendor/composer/../laravel/framework/src/Illuminate/Foundation/Testing/TestCase.php
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => '1728100975-v4',
+   'data' => false,
+));
---
+++ tmp/cache/PHPStan/c4/44/c444fd50279f8741b8628cc53da20c99aa55b43c.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types = 1);
+
+// odsl-/Users/yi.wang/Practices/laravel-all-in-one/tests
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    '/Users/yi.wang/Practices/laravel-all-in-one/tests/Unit/ExampleTest.php' => 
+    array (
+      0 => '91a952d5ada8d3d11d059d9ad547e83f37785842',
+      1 => 
+      array (
+        0 => 'tests\\unit\\exampletest',
+      ),
+      2 => 
+      array (
+        0 => 'tests\\unit\\test_that_true_is_true',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/Users/yi.wang/Practices/laravel-all-in-one/tests/CreatesApplication.php' => 
+    array (
+      0 => '1c178c8519b41c9b4d0c6296848b354376ff38a1',
+      1 => 
+      array (
+        0 => 'tests\\createsapplication',
+      ),
+      2 => 
+      array (
+        0 => 'tests\\createapplication',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/Users/yi.wang/Practices/laravel-all-in-one/tests/TestCase.php' => 
+    array (
+      0 => '895ec7a34b6905c178902ce310866a0702fdbec7',
+      1 => 
+      array (
+        0 => 'tests\\testcase',
+      ),
+      2 => 
+      array (
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/Users/yi.wang/Practices/laravel-all-in-one/tests/Feature/ExampleTest.php' => 
+    array (
+      0 => '20fb4281ed2dc142624c4b61bf49e5ab347b73ff',
+      1 => 
+      array (
+        0 => 'tests\\feature\\exampletest',
+      ),
+      2 => 
+      array (
+        0 => 'tests\\feature\\test_the_application_returns_a_successful_response',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
