f8af45389fe4ce1669db03962115c45c75785083
Yi Wang <wangyiicloud@icloud.com>
2025-02-26 18:39:52 +0800

    Override illuminate FormRequest
    # Conflicts:
    #	app/Http/Kernel.php

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+class BaseFormRequest extends FormRequest
+{
+    protected function failedValidation(Validator $validator):never
+    {
+        throw new UnProcessableEntityHttpException($validator->errors()->first());
+    }
+}
